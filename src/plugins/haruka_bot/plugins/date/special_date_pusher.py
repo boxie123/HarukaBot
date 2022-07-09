3d563faefbc91df7ee8d13d10c5e25129aeb47b1
铂屑 <1824390830@qq.com>
2022-07-10 01:40:37 +0800

    Update special_date_pusher.py

--- src/plugins/haruka_bot/plugins/date/special_date_pusher.py
+++ src/plugins/haruka_bot/plugins/date/special_date_pusher.py
@@ -25,22 +25,12 @@ async def sd_sched(is_week: bool = True):
             message="定时温馨提醒：\n" + message,
             at=False
         )
-    for push in push_list:
-        logger.info("定时任务 special_date_sched 执行，发送消息给{}".format(push["type_id"]))
-        await safe_send(
-            bot_id=3283903771,
-            # bot_id=1031515006,
-            send_type=push["type"],
-            type_id=push["type_id"],
-            message="定时温馨提醒：\n" + message,
-            at=False,
-        )
 
 scheduler.add_job(
     sd_sched, "cron", args=(True,), day_of_week=0, hour=0, minute=3, second=0, id="special_date_sched_week",
-    # sd_sched, "cron", args=(True,), day_of_week=5, hour=16, minute=8, second=30, id="special_date_sched_week",
+    # sd_sched, "cron", args=(True,), day_of_week=6, hour=1, minute=39, second=0, id="special_date_sched_week",
 )
 scheduler.add_job(
     sd_sched, "cron", args=(False,), day=1, hour=0, minute=3, second=0, id="special_date_sched_month",
-    # sd_sched, "cron", args=(False,), day_of_week=5, hour=16, minute=9, second=0, id="special_date_sched_month",
+    # sd_sched, "cron", args=(False,), day=10, hour=1, minute=39, second=30, id="special_date_sched_month",
 )
