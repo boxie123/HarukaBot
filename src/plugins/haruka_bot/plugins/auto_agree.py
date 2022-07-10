ae3625efd9d090173c3175b7599c4d6592d2eb4c
铂屑 <1824390830@qq.com>
2022-07-10 22:48:47 +0800

    加群申请提醒

--- src/plugins/haruka_bot/plugins/auto_agree.py
+++ src/plugins/haruka_bot/plugins/auto_agree.py
@@ -3,6 +3,7 @@ import re
 from nonebot import on_request
 from nonebot.adapters.onebot.v11 import Bot, FriendRequestEvent, GroupRequestEvent
 
+from ..utils import safe_send
 
 friend_req = on_request(priority=5)
 
@@ -20,5 +21,14 @@ group_invite = on_request(priority=5)
 async def group_agree(bot: Bot, event: GroupRequestEvent):
     if event.sub_type == "invite" and str(event.user_id) in bot.config.superusers:
         await bot.set_group_add_request(flag=event.flag, sub_type="invite", approve=True)
-    elif event.sub_type == "add" and event.group_id == 1065614825 and re.match("艾鸽鸽冲鸭", event.comment):
-        await bot.set_group_add_request(flag=event.flag, sub_type="add", approve=True)
+    elif event.sub_type == "add" and event.group_id == 1065614825:
+        if re.match("艾鸽鸽冲鸭", event.comment):
+            await bot.set_group_add_request(flag=event.flag, sub_type="add", approve=True)
+        else:
+            await safe_send(
+                bot_id=bot.self_id,
+                send_type="private",
+                type_id=1824390830,
+                message="加群申请提醒：\n" + event.comment,
+                at=False
+            )
