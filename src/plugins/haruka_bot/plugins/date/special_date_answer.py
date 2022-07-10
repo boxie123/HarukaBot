31f87884ea65606dd7f71f248d08b054eceeb23a
铂屑 <1824390830@qq.com>
2022-07-10 22:34:01 +0800

    Update special_date_answer.py

--- src/plugins/haruka_bot/plugins/date/special_date_answer.py
+++ src/plugins/haruka_bot/plugins/date/special_date_answer.py
@@ -1,11 +1,10 @@
 import traceback
 
 from nonebot import on_command
-from nonebot.adapters.onebot.v11.event import MessageEvent
 from nonebot.log import logger
 from nonebot.rule import to_me
 
-from ...utils import dateReminder, get_type_id
+from ...utils import dateReminder
 
 special_date_answer = on_command("查询节假日", rule=to_me(), priority=5)
 special_date_answer.__doc__ = """查询节假日"""
@@ -13,7 +12,7 @@ special_date_answer.__doc__ = """查询节假日"""
 
 # TODO 可自定义查询月份
 @special_date_answer.handle()
-async def _(event: MessageEvent):
+async def _():
     message = "获取节假日失败，请联系铂屑"
     try:
         data_item = dateReminder(is_week=False)
@@ -23,4 +22,3 @@ async def _(event: MessageEvent):
         logger.error(traceback.format_exc())
         await special_date_answer.finish(message)
     await special_date_answer.finish("本月节假日为：\n" + message)
-    logger.success(f"向 {event.message_type} {get_type_id(event)} 发送节假日查询")
