8c24611c4edeed0d6468dc8feeca409ef3db00cb
铂屑 <1824390830@qq.com>
2022-07-31 18:44:48 +0800

    改用arg

--- src/plugins/haruka_bot/plugins/date/special_date_add.py
+++ src/plugins/haruka_bot/plugins/date/special_date_add.py
@@ -1,8 +1,8 @@
 from nonebot import on_command
 from nonebot.adapters import Message
-from nonebot.params import CommandArg
+from nonebot.matcher import Matcher
+from nonebot.params import CommandArg, ArgPlainText
 from nonebot.rule import to_me
-from nonebot.typing import T_State
 
 from ...database import DB as db
 from ...utils import permission_check
@@ -14,27 +14,26 @@ special_date_add.handle()(permission_check)
 
 
 @special_date_add.handle()
-async def handle_month(
-        state: T_State,
+async def handle_date(
+        matcher: Matcher,
         command_arg: Message = CommandArg(),
 ):
     special_day = command_arg.extract_plain_text().split()
-    if special_day[1].isdecimal() and special_day[2].isdecimal():
-        state["month"] = special_day[1]
-        state["day"] = special_day[2]
-        state["name"] = special_day[0]
+    if len(special_day) == 3 and special_day[1].isdecimal() and special_day[2].isdecimal():
+        matcher.set_arg("date", command_arg)
     else:
         await special_date_add.finish("month 和 day 应为纯阿拉伯数字")
 
 
-@special_date_add.handle()
-async def _(state: T_State):
+@special_date_add.got("date", prompt="请输入要添加的特殊日期")
+async def _(sp_date: str = ArgPlainText("date")):
     """手动添加特殊日期"""
+    special_day = sp_date.split()
     result = await db.add_special(
-        name=state["name"],
-        month=state["month"],
-        day=state["day"],
+        name=special_day[0],
+        month=special_day[1],
+        day=special_day[2],
     )
     if result:
-        await special_date_add.finish(f"已添加特殊日期\"{state['name']}\"")
+        await special_date_add.finish(f"已添加特殊日期\"{special_day[0]}\"")
     await special_date_add.finish("添加特殊日期失败，请联系铂屑")
